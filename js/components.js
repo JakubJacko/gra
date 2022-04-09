edad6a578fb2634e984970775ae31fea086a5c94
JakubJacko <jakub.jacko@op.pl>
2022-04-09 12:16:41 +0200

    pl1

---
+++ js/components.js
@@ -0,0 +1,12 @@
+class Player {
+    constructor(x, y, w, h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        
+    }
+}
+
+const pl1 = new Player(10, 0, 1, 2);
+console.log(pl1);
